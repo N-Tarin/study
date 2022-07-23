d28d0e3d0cca68b4b4300a48e449c2f3dcee0f4d
N-Tarin <karinuxxx1029@gmail.com>
2022-07-23 15:44:12 +0900

    delete Footer

--- components/templates/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <footer>
-    <h2>Footer</h2>
-  </footer>
-</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,15 @@
   <div>
     <Header />
     <nuxt />
-    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '../components/templates/Header.vue'
-import Footer from '../components/templates/Footer.vue'
 
 export default {
   components: {
     Header,
-    Footer,
   },
 }
 </script>
